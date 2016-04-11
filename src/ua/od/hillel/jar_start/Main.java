e0d1b0244acc58e866472d8f1f07d108c7263b10
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2016-04-11 20:06:33 +0300

    commit 1

--- src/ua/od/hillel/jar_start/Main.java
+++ src/ua/od/hillel/jar_start/Main.java
@@ -3,6 +3,10 @@ package ua.od.hillel.jar_start;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int i = 0;
+
+        while (i < 5) {
+            System.out.println(i);
+        }
     }
 }
